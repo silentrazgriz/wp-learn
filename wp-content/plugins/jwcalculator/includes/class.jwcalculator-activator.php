25af86d4083cd9b5b1d59bf40cd0c47328ae3561
silentrazgriz <silentrazgriz@gmail.com>
2017-08-02 19:28:30 +0700

    working on plugins

---
+++ wp-content/plugins/jwcalculator/includes/class.jwcalculator-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://ashiwawa.com/
+ * @since      1.0.0
+ *
+ * @package    Jwcalculator
+ * @subpackage Jwcalculator/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Jwcalculator
+ * @subpackage Jwcalculator/includes
+ * @author     vekaputra <silentrazgriz@gmail.com>
+ */
+class Jwcalculator_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wp-content/plugins/jwcalculator/includes/class.jwcalculator-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://ashiwawa.com/
+ * @since      1.0.0
+ *
+ * @package    Jwcalculator
+ * @subpackage Jwcalculator/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Jwcalculator
+ * @subpackage Jwcalculator/includes
+ * @author     vekaputra <silentrazgriz@gmail.com>
+ */
+class Jwcalculator_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
